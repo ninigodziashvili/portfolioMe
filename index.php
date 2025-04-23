74de46e372af1f82b52aed19a127215414cc1d26
Nino Godziashvili <nino.godziashvili@gmail.com>
2025-04-24 00:14:51 +0200

    create some structure changes for the projject

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include 'includes/header.php'; ?>
+
+<main>
+  <?php include 'sections/about.php'; ?>
+  <?php include 'sections/experience.php'; ?>
+  <?php include 'sections/work.php'; ?>
+  <?php include 'sections/contact.php'; ?>
+</main>
+
+<?php include 'includes/footer.php'; ?>
