e6165f89c5ef913a471d8faed7f00c8976ef766b
ninigodziashvili <nino.godziashvili@gmail.com>
2025-05-09 12:18:45 +0200

    delete unneccecary code

--- front-page.php
+++ front-page.php
@@ -1,2 +1,11 @@
 <?php get_header(); ?>
+<main class="main">
+    <h2>Hola, soy <br><br><span>Nino Godziashvili</span></h2>
+    <p>Construyo cosas para la web.</p>
+    <p>Soy ingeniero de software especializado en la creación (y ocasionalmente el diseño) 
+      de experiencias digitales excepcionales. Actualmente, me centro en la creación de productos accesibles y centrados en el ser humano.</p>
+      </div>
+      <div><a class="click-btn btn-style5" href="https://www.linkedin.com/in/nino-godziashvili-05874a163" target="_blank">LinkedIn</a></div>
+    </div>
+  </main>
     <?php get_footer(); ?>
