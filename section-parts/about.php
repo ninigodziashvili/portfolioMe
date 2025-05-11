635372b50517d78b36aee066f9c6e4d9584374d3
Nino Godziashvili <nino.godziashvili@gmail.com>
2025-05-11 23:20:38 +0200

    create about me section

--- section-parts/about.php
+++ section-parts/about.php
@@ -2,19 +2,38 @@
 <h2 class="about-me"><span>Sobre mi</span></h2>
 <div class="about-me-info">
 <div class="about-me-info-txt">
-<p>¡Hola! Me llamo Nino y disfruto creando soluciones digitales funcionales. Mi interés por el desarrollo comenzó hace años, 
+<p>Disfruto creando soluciones digitales funcionales. Mi interés por el desarrollo comenzó hace años, 
     cuando descubrí lo emocionante que era resolver problemas técnicos y diseñar experiencias intuitivas para los usuarios.
   </p><br>
 <p>He tenido la oportunidad de trabajar en bancos, aseguradoras, academias tecnológicas, y también en proyectos propios. 
 </p>
-<p><br>
+<!-- <p><br>
 He asumido distintos roles, 
   desde soporte técnico hasta liderazgo de capítulos de testing y desarrollo de apps móviles.
    Estoy cursando estudios de desarrollo web y aprendiendo español para ampliar mis horizontes profesionales en España.
-Aquí algunos lenguajes y herramientas con los que he estado trabajando últimamente:</p>
+Aquí algunos lenguajes y herramientas con los que he estado trabajando últimamente:</p> -->
 </div>
-<div class="profile-img-container">
-<img src="http://secondtry.local/wp-content/uploads/2025/05/CV_photo.jpeg" alt="">
+
+<div class="image-border">
+  <div class="main-image"></div>
+  <p class="title">Image Title</p>
 </div>
-</div>
+
 </section>
+<!-- <div class="card-container">
+  <div class="card card--dark">
+    <a href="#phonesoff">
+      <div class="card--display">
+        <i class="material-icons">ring_volume</i>
+        <h2>Why Phones Are So Loud</h2>
+      </div>
+      <div class="card--hover">
+        <h2>Why Phones Are So Loud</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at est orci. Nam molestie pellentesque mi nec lacinia.</p>
+        <p class="link">Click to see project</p>
+      </div>
+    </a>
+    <div class="card--border"></div>
+  </div>
+</div>
+ -->
